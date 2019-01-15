9d69d2de46c2c8b278961563d823a5c2444cb1fc
Alec <townsend.alec@gmail.com>
2019-01-15 01:31:18 -0600

    Apps 1-7 complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,19 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(){
+    super();
+    this.state = {
+      filterString: "",
+      smashMains: [
+      "Palutena",
+      "Lucina",
+      "ZSS",
+      "Corrin",
+      "Ike"
+      ]
+    }
   }
+
+  handleChange(filter){
+    this.setState({filterString: filter})
+  }
+  render() {
+    let mainsToShow = this.state.smashMains.filter((e,index) => {
+      return e.includes(this.state.filterString);
+    }).map((e,index) => {
+ return <h2 key = {index}> {e} </h2>
+  })
+ 
+return (
+  <div className = "App">
+    <input onChange = {(e) => this.handleChange(e.target.value)} type="text"/>
+  {mainsToShow}
+  </div>
+);
+}
 }
 
 export default App;
--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Image from "./Img";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className= "App">
+        <Image myImage = {"https://static1.fjcdn.com/comments/Gtmarios+fw+he+sees+luigi+in+mario+kart+8+_6394788a5bf7139122bc58ff4d8a521c.png"}/>
       </div>
     );
   }
